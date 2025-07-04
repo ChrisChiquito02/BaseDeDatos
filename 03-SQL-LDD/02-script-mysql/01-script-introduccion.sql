b4db8fb37177402806de66c4683481aa5603b0c7
Christian Isaac Martínez Hernández <chrische0708@gmail.com>
2025-07-04 11:57:29 -0600

    "Logro: Creación de tablas con razones de cardinalidad 1:N, 1:1,M:N, claves primarias, compuestas y claves fóraneas compuestas."

--- 03-SQL-LDD/02-script-mysql/01-script-introduccion.sql
+++ 03-SQL-LDD/02-script-mysql/01-script-introduccion.sql
@@ -1,30 +1,29 @@
---Lenguaje SQL-LDD (create, alter y drop)
+# Lenguaje SQL-LDD (create, alter y drop)
 
---Crear la base de datos empresag2
+# Crear la base de datos empresag2
 CREATE DATABASE empresag2;
-GO
 
---utilizar la base de datos
+
+# Utilizar la base de datos
 USE empresag2;
 
---crear la tabla categorias
+# Crear la tabla categorias
 CREATE TABLE categorias
 (
 	CategoriaId int primary key,
 	nombrecatgeoria nvarchar(30) not null unique
 );
-GO
 
---crear tabla empleado
+#crear tabla empleado
 CREATE TABLE empleado
 (
-EmpleadoId int not null identity(1,1),
+EmpleadoId int not null auto_increment,
 	Nombre nvarchar(20) not null,
 	ApellidoPaterno nvarchar(15) not null,
 	curp char(10) not null,
 	telefono char(15),
 	sexo char(1) not null,
-	activo bit not null,
+	activo boolean not null,
 	CONSTRAINT pk_empleado
 	PRIMARY KEY (EmpleadoId),
 	CONSTRAINT unique_curp
@@ -32,29 +31,31 @@ EmpleadoId int not null identity(1,1),
 	CONSTRAINT chk_sexo
 	CHECK (sexo in ('M','F'))
 );
-GO
 
---Insertar en categorias
+DROP TABLE empleado;
+
+# Insertar en categorias
 INSERT INTO categorias(CategoriaId, nombrecatgeoria)
 VALUES (1,'CARNES FRIAS');
-GO
+
 
 INSERT INTO categorias(CategoriaId, nombrecatgeoria)
 VALUES (2,'VINOS Y LICORES');
-GO
+
 
 SELECT *FROM categorias;
-GO
---Insertar en empleado
+
+# Insertar en empleado
 INSERT INTO empleado (Nombre, ApellidoPaterno,curp, telefono, sexo, activo)
 VALUES ('Monico', 'Panfilo', 'asdsadas', '123456789', 'M', 1);
-GO
+
 
 INSERT INTO empleado (Nombre, ApellidoPaterno,curp, telefono, sexo, activo)
 VALUES ('Sarai', 'Vitoria', 'aksjdhas', '987654321', 'F', 1);
-GO
 
--- Creacion de primary keys compuestas
+SELECT *FROM empleado;
+
+#  Creacion de primary keys compuestas
 
 CREATE TABLE Tabla1
 (
@@ -64,13 +65,12 @@ CREATE TABLE Tabla1
 	CONSTRAINT pk_tabla1 
 	PRIMARY KEY (Tabla1id1, Tabla1id2)
 );
-GO
 
--- Razon de cardinalidad 1:N
+# Razon de cardinalidad 1:N
 
 CREATE TABLE Tabla2
 (
-	Tabla2id int not null identity(1,1),
+	Tabla2id int not null auto_increment,
 	Nombre varchar(20),
 	Tabla1id1 int,
 	Tabla1id2 int,
@@ -80,19 +80,17 @@ CREATE TABLE Tabla2
 	FOREIGN KEY (Tabla1id1, Tabla1id2)
 	REFERENCES Tabla1(Tabla1id1,Tabla1id2)
 );
-GO
 
-
---Razón de cardinalidad de 1:1 (Fidelidad)
+# RazÃ³n de cardinalidad de 1:1 (Fidelidad)
 
 CREATE TABLE Employee
 (
-	Id int not null identity(1,1),
+	Id int not null auto_increment,
 	Nombre varchar(20) not null,
 	Ap1 varchar(15) not null,
 	Ap2 varchar(15),
 	Sexo char(1) not null,
-	Salario money not null
+	Salario numeric(10,2) not null,
 	CONSTRAINT pk_employee
 	PRIMARY KEY (Id), 
 	CONSTRAINT chk_Sexo2
@@ -100,11 +98,10 @@ CREATE TABLE Employee
 	CONSTRAINT chk_salario
 	CHECK (Salario>0.0)
 );
-GO
 
 CREATE TABLE Department
 (
-	Id int not null identity(1,1),
+	Id int not null auto_increment,
 	NombreProyecto varchar(20) not null,
 	Ubicacion varchar(15) not null,
 	FechaInicio date not null,
@@ -119,18 +116,16 @@ CREATE TABLE Department
 	FOREIGN KEY (IdEmployee)
 	REFERENCES Employee(Id)
 );
-GO
 
---Crear tabla project (M:N)
+# Crear tabla project (M:N)
 
 CREATE TABLE Project
 (
-	ProjectId int not null identity(1,1),
+	ProjectId int not null auto_increment,
 	nombreProject varchar(20),
 	CONSTRAINT pk_project
 	PRIMARY KEY (ProjectId)
 );
-GO
 
 CREATE TABLE Works_On
 (
@@ -146,4 +141,4 @@ CREATE TABLE Works_On
 	FOREIGN KEY (projectId)
 	REFERENCES Project(ProjectId)
 );
-GO
+
